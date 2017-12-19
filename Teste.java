1b58fd5adb7d42f87601c4b3b75b91bd9258ffe3
DjibJoe <34548056+DjibJoe@users.noreply.github.com>
2017-12-19 10:02:29 +0100

    temps = +de 3 mins

--- Teste.java
+++ Teste.java
@@ -1,16 +1,13 @@
 package com.test;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 public class Teste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int [] itemsToTest= {1,2,0,
+		int [] itemsToTest= {2,1,0,
 							 4,5,6,
-							 7,3,8};
+							 3,7,8};
 		long dbt,fin,secondes,munites=0;
 		Puzzle taquin=new Puzzle();
 		Noeud debut=new Noeud(itemsToTest, null);
@@ -27,7 +24,7 @@ public class Teste {
 		
 		
 		
-		System.out.println("temps d\'execution : "+munites+" munites " +secondes+" s et "+(fin-dbt)%1000+ "  milliseconde"); // 134 s 263  milliseconde
+		System.out.println("temps d\'execution : "+munites+" munites " +secondes+" s et "+(fin-dbt)%1000+ "  milliseconde"); // 3 munites 18 s et 909  milliseconde
 		
 		
 	
